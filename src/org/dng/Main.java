2aba9f036701ff2d74d03c4150b1d3ccbd8c307a
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2022-09-05 19:48:53 +0300

    add file text.txt to resources and make filtering of sentences

--- src/org/dng/Main.java
+++ src/org/dng/Main.java
@@ -1,12 +1,16 @@
 package org.dng;
 
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
         //start servers factory
-        ServerFactory.createServers();
+        //ServerFactory.createServers();
 
-        //System.out.println(RaveGenerator.getSentance());
+//        URL url = Main.class.getClassLoader().getResource("resources/text.txt");
+//        System.out.println(url.getPath());
+        System.out.println(RaveGenerator.getSentance());
 
 
     }
--- src/org/dng/RaveGenerator.java
+++ src/org/dng/RaveGenerator.java
@@ -3,53 +3,43 @@ package org.dng;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.net.URL;
+import java.util.*;
 
 public class RaveGenerator {
-    private static final String fileName = "d:\\text.txt";
+//    private static final String fileName = "d:\\text.txt";
+
+//    getResource("resources/text.txt");
     private static List<String> sentenceList=new ArrayList<>();
 
     static {
-        try( BufferedReader br = new BufferedReader(
-                new FileReader(fileName))
+        URL url = RaveGenerator.class.getClassLoader().getResource("resources/text.txt");
+        try(
+                BufferedReader br = new BufferedReader(
+//                new FileReader(fileName))
+                new FileReader(url.getPath()))
         ) {
+
             String text = null;
             LinkedList<String> ll = new LinkedList<>();
             while ((text=br.readLine())!=null){
-                final String sentences[] = text.split("[.!?]\\s*");
-                for (int i = 0; i < sentences.length; ++i) {
-                    ll.add(sentences[i]);
-                }
+//                final String sentences[] = text.split("[.!?]\\s*");
+//                for (int i = 0; i < sentences.length; ++i) {
+//                    ll.add(sentences[i]);
+//                }
+
+                //lets to filter sentence
+                final LinkedList<String> lll = new LinkedList<>(Arrays.stream(text.split("[.!?]\\s*"))
+                        .map(s -> s.trim())
+                        .filter(s -> s.length()>0)
+                        .toList());
+                ll.addAll(lll);
             }
             sentenceList=ll.stream().toList();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
-//    private static @NotNull List<String> getSentenceList(){
-//
-//        List<String> sentenceList = new ArrayList<>();
-//
-//        try( BufferedReader br = new BufferedReader(
-//                new FileReader(fileName))
-//        ) {
-//            String text = null;
-//            while ((text=br.readLine())!=null){
-//                final String sentences[] = text.split("[.!?]\\s*");
-//                for (int i = 0; i < sentences.length; ++i) {
-//                    sentenceList.add(sentences[i]);
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return sentenceList;
-//    }
 
     public static String getSentance(){
         Random random = new Random();
